c24fcc8b261e9035031d0a0b84c022ab406e290b
Ujjwal Kalkhande <58893950+ujjwalbb30@users.noreply.github.com>
2021-08-12 15:39:49 -0400

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,6 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   echo("inside if")
