104d5eec0a304d9a0757a5123af3ce2a41f316a8
Ujjwal Kalkhande <58893950+ujjwalbb30@users.noreply.github.com>
2021-08-12 16:21:22 -0400

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 //   echo("inside if")
@@ -13,7 +12,6 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-echo("$name   $email")
 // Create the email and send the message
 $to = "ujjwaltech.05@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
